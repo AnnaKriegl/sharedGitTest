36ca383dec2d74821624da828f2962ab66fd1898
AnnaKriegl <mail@anna-kriegl.de>
2013-07-01 16:24:57 +0200

    some cool changes just that you know

--- createCube.py
+++ createCube.py
@@ -1,6 +1,7 @@
 import maya.cmds as mc
+from functools import partial
 
-def polyCubeRange(xx):
+def polyCubeRange(xx): #RAANGER
     for x in range(xx):
         mc.polyCube()
 
@@ -8,5 +9,23 @@ def polyPrismRange(xx):
     for x in range(xx):
         mc.polyPrism()
 
+print "hello octocat"
+
 def createSphere():
     mc.polySphere()
+
+print "how does this work?!?!?!?!?!??!?!"
+
+def sumyInYourTummy(foodA, foodB):
+    return foodA + foodB
+
+partial(sumyInYourTummy, 1, 2)
+
+f = 10
+
+def makeSunshine():
+    clouds = False
+    if not clouds:
+        sun = True
+        return sun
+
